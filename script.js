ac3e737a9ede79b83b14c5b513b2d05e166844dd
woolisbest-4520 <woolisbest.4520@gmail.com>
2025-09-02 11:07:48 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+let player;
+
+function loadYouTubeAPI() {
+    const script = document.createElement('script');
+    script.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(script);
+}
+
+// YouTubeのプレイヤーを初期化
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '450',
+        width: '800',
+        videoId: 'dQw4w9WgXcQ', // デフォルトの動画ID
+    });
+}
+
+// 動画URLを入力してプレイヤーを更新
+function loadVideo() {
+    const url = document.getElementById('videoUrl').value;
+    const videoId = getVideoIdFromUrl(url);
+    if (videoId) {
+        player.loadVideoById(videoId);
+    } else {
+        alert('無効なYouTube URLです');
+    }
+}
+
+// YouTubeのURLから動画IDを抽出
+function getVideoIdFromUrl(url) {
+    const match = url.match(/(?:https?:\/\/)?(?:www\.)?youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)([^"&?/ ]{11})/);
+    return match ? match[1] : null;
+}
+
+loadYouTubeAPI();
